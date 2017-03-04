bd7a79e7522fac89adaf30e2407eff25e492ae31
Alejandro Druetta <aledruetta@gmail.com>
2017-03-03 22:35:01 -0300

    anuncios admin customization

--- anuncios/admin.py
+++ anuncios/admin.py
@@ -4,7 +4,14 @@ from .models import Ciudad
 from .models import TipoDomicilio
 from .models import TipoPropiedad
 
-admin.site.register(Anuncio)
+
+class AnuncioAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'tipoProp', 'tipoTrans', 'ciudad')
+    list_filter = ('creado', 'activo', 'destacado')
+    search_fields = ('titulo', 'descripcion')
+
+
+admin.site.register(Anuncio, AnuncioAdmin)
 admin.site.register(Ciudad)
 admin.site.register(TipoDomicilio)
 admin.site.register(TipoPropiedad)
