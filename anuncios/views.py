f15bc639bfa36c4f846e77e1f7e372b46e77b590
Alejandro Druetta <aledruetta@gmail.com>
2017-03-05 20:05:06 -0300

    templates

--- anuncios/views.py
+++ anuncios/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from .models import Anuncio
+from .models import Anuncio, Ciudad, TipoPropiedad
 
 
 def home(request):
     anuncios = Anuncio.objects.all()
     anuncios_dest = anuncios.filter(destacado=True).order_by('-creado')
-    context = {'anuncios': anuncios_dest}
+    ciudades = Ciudad.objects.all()
+    propiedades = TipoPropiedad.objects.all()
+    context = {
+        'anuncios': anuncios_dest,
+        'ciudades': ciudades,
+        'propiedades': propiedades
+    }
     return render(request, 'anuncios/index.html', context)
