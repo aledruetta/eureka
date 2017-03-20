4b5956fdee8d48551e4a5cedb1ea0fadbc878ab8
Alejandro Druetta <aledruetta@gmail.com>
2017-03-20 10:49:27 -0300

    comentarios en views.py

--- anuncios/views.py
+++ anuncios/views.py
@@ -4,55 +4,62 @@ import locale
 
 
 def home(request):
+    # Objetos Base de Datos
     anuncios = Anuncio.objects.all()
-    anuncios_dest = anuncios.filter(
-        destacado=True,
+    destacados = anuncios.filter(
         activo=True,
-    ).order_by('-creado')[:4]
-
+        destacado=True,
+    ).order_by('-creado')[:5]
     ciudades = Ciudad.objects.all()
-    propiedades = TipoPropiedad.objects.all()
+    propiedades_tipo = TipoPropiedad.objects.all()
 
     anuncios_procesados = list()
 
-    for anuncio in anuncios_dest:
+    for anuncio in destacados:
+        # Domicilio
         altura = anuncio.numero - (anuncio.numero % 100)
 
-        prep = ''
+        prepos = ''
         if altura > 0:
-            prep = 'al '
+            prepos = 'al '
         else:
             altura = ''
 
         domicilio = '{tipo} {nombre} {prep}{altura}'.format(
             tipo=anuncio.tipoDom,
             nombre=anuncio.domicilio.title(),
-            prep=prep,
+            prep=prepos,
             altura=altura,
         )
 
+        # Terreno
         if anuncio.unidad == 'M':
             unidad = 'm²'
         elif anuncio.unidad == 'H':
             unidad = 'Ha'
 
+        # Transacción
         if anuncio.tipoTrans == 'V':
             tipoTrans = 'Venta'
         elif anuncio.tipoTrans == 'A':
             tipoTrans = 'Alquiler'
 
+        # Pileta
         if anuncio.pileta:
             pileta = 'Sí'
         else:
             pileta = 'No'
 
+        # Cochera
+        cochera_base = str(anuncio.cochera) + 'vehículo'
         if anuncio.cochera == 1:
-            cochera = '1 vehículo'
+            cochera = cochera_base
         elif anuncio.cochera > 1:
-            cochera = str(anuncio.cochera) + ' vehículos'
+            cochera = cochera_base + 's'
         else:
             cochera = 'No'
 
+        # Moneda
         locale.setlocale(locale.LC_ALL, 'pt_BR.utf-8')
         precio = locale.currency(anuncio.precio, grouping=True)
 
@@ -74,10 +81,12 @@ def home(request):
             'descripcion':  anuncio.descripcion,
         })
 
+    # Diccionario de objetos pasados para la renderización
     context = {
         'anuncios':    anuncios_procesados,
         'ciudades':    ciudades,
-        'propiedades': propiedades
+        'propiedades': propiedades_tipo,
     }
 
+    # Renderiza template y retorna HTTPResponse
     return render(request, 'anuncios/index.html', context)
